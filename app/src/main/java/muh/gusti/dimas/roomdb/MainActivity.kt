d1ba1cf5d1e810331c84f4a1e460fa9cf5035798
gustidimass <gustidimas11@gmail.com>
2021-05-25 19:34:47 +0800

    connect viewmodel with activity

--- app/src/main/java/muh/gusti/dimas/roomdb/MainActivity.kt
+++ app/src/main/java/muh/gusti/dimas/roomdb/MainActivity.kt
@@ -1,11 +1,29 @@
 package muh.gusti.dimas.roomdb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import muh.gusti.dimas.roomdb.R
+import muh.gusti.dimas.roomdb.Note
+import kotlinx.android.synthetic.main.activity_main.*
+
+private lateinit var noteViewModel: NoteViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+        noteViewModel.getNotes()?.observe(this, Observer {
+            noteAdapter.setNotes(it)
+        })
+
     }
 }
